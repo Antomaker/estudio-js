50af433e784ee65f62bbfa7712057dad94cac7db
Antomaker <96790352+Antomaker@users.noreply.github.com>
2026-02-09 14:22:00 -0500

    Update script.js

--- CPJSM/script.js
+++ CPJSM/script.js
@@ -1,1 +1,34 @@
 console.log('Hello World')
+//comentario
+/*
+multi commentario
+
+*/ 
+// TODO:
+//! BUG
+//? COMMENTS
+
+let mutable=0;
+const inmutable=0;
+console.log();
+
+//1. Numbers
+
+const stars=100000; 
+const pi=3.14;
+
+//2. Strings 
+const message="hello world!";
+
+//3 boolean
+const isBaked= false;
+const isBurnt= false;
+
+//4 Null
+let reservation= null;
+
+//5 undefined
+let myNote;
+console.log(myNote);
+
+//6 Sym
