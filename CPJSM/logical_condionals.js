d3befdc8acf991a78284b220e5ac11fdb1da27fa
AJDeLavalle <96790352+Antomaker@users.noreply.github.com>
2026-04-29 02:22:12 -0500

    cambios

---
+++ CPJSM/logical_condionals.js
@@ -0,0 +1,24 @@
+//logical and conditionals
+
+const age = 18;
+
+if (age >= 18) {
+   console.log('You may enter, welcome!');
+}
+const age = 18;
+
+if (age >= 18) {
+    console.log('You may enter, welcome!');
+} else if (age === 18) {
+    console.log('You just turned 18, welcome!');
+}
+
+const age = 18;
+
+if (age > 18) {
+    console.log('You may enter, welcome!');
+} else if (age === 18) {
+    console.log('You just turned 18, welcome!');
+} else {
+    console.log('Go away!');
+}
